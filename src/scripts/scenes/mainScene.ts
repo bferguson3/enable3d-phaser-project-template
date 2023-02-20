0075f6e46a8a5953a4a1185cdd3cbdc1bab6bd03
Ben Ferguson <ferguson.ben86@gmail.com>
2023-02-19 21:39:11 -0500

    phaser3-enable3d --> cobble

--- src/scripts/scenes/mainScene.ts
+++ src/scripts/scenes/mainScene.ts
@@ -1,27 +1,89 @@
-import { Scene3D } from '@enable3d/phaser-extension'
+import { Scene3D } from "@enable3d/phaser-extension";
+import * as THREE from "three";
 
-export default class MainScene extends Scene3D {
-  constructor() {
-    super({ key: 'MainScene' })
-  }
+export default class MainScene extends Scene3D 
+{
+	private newMat : THREE.ShaderMaterial;
+	private cube : THREE.Mesh;
 
-  init() {
-    this.accessThirdDimension()
-  }
+	constructor() 
+	{
+		super({ key: "MainScene" });
+	}
 
-  create() {
-    // creates a nice scene
-    this.third.warpSpeed()
+	init() : void 
+	{
+		this.accessThirdDimension();
+	}
 
-    // adds a box
-    this.third.add.box({ x: 1, y: 2 })
+	create() : void
+	{
+		// scene template
+		this.third.warpSpeed();
 
-    // adds a box with physics
-    this.third.physics.add.box({ x: -1, y: 2 })
+		const vertexSrc = `
+        varying vec2 v_uv;
+        float dist;
+        
+        void main() {
+            v_uv = uv;
+            
+            vec4 cs = modelViewMatrix * vec4(position, 1.0f);
+            
+            dist = -cs.z;
+            dist = 128.0f / dist;
+            
+            vec4 t = projectionMatrix * cs;
+            
+            t.x = floor(t.x * dist)/dist;
+            t.y = floor(t.y * dist)/dist;
+            t.z = floor(t.z * dist)/dist;
+            v_uv.x = floor(v_uv.x * dist)/dist;
+            v_uv.y = floor(v_uv.y * dist)/dist;
+           
+            gl_Position = t; //projectionMatrix * cs;
+        }`;
 
-    // throws some random object on the scene
-    this.third.haveSomeFun()
-  }
+		const fShader = `
+        varying vec2 v_uv;
+        uniform sampler2D map;
+        uniform float u_time;
+        
+        void main() {
+            
+            vec4 tex = texture2D( map, v_uv );
+            
+            gl_FragColor = tex;
+            
+        }
+        `;
 
-  update() {}
+		const uniforms2 = {
+			u_time: { value: 0.0 },
+			map: { value: null }
+		};
+		const grass = new THREE.TextureLoader().load("assets/img/phaser-logo.png");
+		grass.minFilter = THREE.NearestFilter;
+		grass.magFilter = THREE.NearestFilter;
+		uniforms2.map.value = grass;
+		
+		this.newMat = new THREE.ShaderMaterial({
+			uniforms: uniforms2,
+			vertexShader: vertexSrc,
+			fragmentShader: fShader,
+			side: THREE.FrontSide,
+		});
+		const geometry = new THREE.BoxGeometry(1.5, 1.5, 1.5);
+		
+		this.cube = new THREE.Mesh(geometry, this.newMat);
+		this.cube.position.set(0, 2.25, 0);
+
+		this.third.physics.scene.add(this.cube);
+		this.third.physics.add.existing(this.cube);
+	}
+
+	update() : void
+	{
+		return;
+	}
 }
--- webpack/credits.js
+++ webpack/credits.js
@@ -1,20 +1,20 @@
 /**
  * PLEASE DO NOT REMOVE THIS NOTICE!
  *
- * @template        This Phaser game was built using phaser-project-template (https://github.com/yandeu/phaser-project-template)
- * @author          Yannick Deubel (https://github.com/yandeu)
- * @copyright       2019 Yannick Deubel
- * @license         {@link https://github.com/yandeu/phaser-project-template/blob/master/LICENSE|MIT License}
+ * @template        This Phaser game was built using phaser-project-template (https://github.com/bferguson3/enable3d-phaser-project-template)
+ * @author          Ben Ferguson (https://github.com/bferguson3/), Yannick Deubel (https://github.com/yandeu)
+ * @copyright       2023 Ben Ferguson, 2019 Yannick Deubel
+ * @license         {@link LICENSE}
  */
 
 // Of course you can remove it if you really want to, but it would be nice if you would leave it there :)
 
 console.log(
-  '%c %c %c %c %c Built using phaser-project-template %c https://github.com/yandeu/phaser-project-template',
-  'background: #ff0000',
-  'background: #ffff00',
-  'background: #00ff00',
-  'background: #00ffff',
-  'color: #fff; background: #000000;',
-  'background: none'
-)
+	"%c %c %c %c %c Built using enable3d-phaser-project-template %c https://github.com/bferguson3/enable3d-phaser-project-template",
+	"background: #ff0000",
+	"background: #ffff00",
+	"background: #00ff00",
+	"background: #00ffff",
+	"color: #fff; background: #000000;",
+	"background: none"
+);
